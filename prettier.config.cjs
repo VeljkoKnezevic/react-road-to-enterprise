0fd7fc065b2e905124050f453ab5ec4f1cea1810
Veljko <veljkoBitno@gmail.com>
2023-07-01 10:08:35 +0200

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "never",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
